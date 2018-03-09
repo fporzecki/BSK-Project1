1384572abb839eeb1fbebed042d9d6d6fbdf8057
Rzecki <filip.porzecki@gmail.com>
2018-03-09 19:07:29 +0100

    initial commit, some work done on GUI

---
+++ BSK-Proj1-GUI/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using BSK_Proj1_Logic;
+using System.ComponentModel;
+using System.Windows;
+
+namespace BSK_Proj1_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _filename;
+
+        private TempClass _tempClass;
+        private BackgroundWorker _backgroundWorker;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DecryptButton.IsEnabled = false;
+            EncryptButton.IsEnabled = false;
+
+            _backgroundWorker = new BackgroundWorker();
+            _tempClass = new TempClass(_backgroundWorker);
+        }
+
+        private void ChooseFileButtom_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new Microsoft.Win32.OpenFileDialog();
+
+            var result = fileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                _filename = fileDialog.FileName;
+                FileNameLabel.Content = _filename;
+                DecryptButton.IsEnabled = true;
+                EncryptButton.IsEnabled = true;
+            }
+        }
+
+        private async void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            _backgroundWorker.WorkerReportsProgress = true;
+            _backgroundWorker.DoWork += (s, args) => _tempClass.EncryptFile();
+            _backgroundWorker.ProgressChanged += new ProgressChangedEventHandler(UpdateProgressChanged);
+            _backgroundWorker.RunWorkerCompleted += (s, args) => MessageBox.Show("Done");
+            _backgroundWorker.RunWorkerAsync();
+        }
+
+        private void UpdateProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProcessProgressBar.Value = (e.ProgressPercentage);
+        }
+    }
+}
---
+++ BSK-Proj1-Logic/TempClass.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Threading;
+
+namespace BSK_Proj1_Logic
+{
+    public class TempClass
+    {
+        private readonly BackgroundWorker _backgroundWorker;
+
+        public TempClass(BackgroundWorker backgroundWorker)
+        {
+            _backgroundWorker = backgroundWorker;
+        }
+
+        public void EncryptFile()
+        {
+            var result = 0;
+            for (var i = 0; i <= 100; i++)
+            {
+                result = i;
+                Thread.Sleep(100);
+                _backgroundWorker.ReportProgress(i);
+            }
+        }
+    }
+}
