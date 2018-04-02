e332e27468556038b77a4166a8ddb8e9237a4611
Rzecki <filip.porzecki@gmail.com>
2018-04-02 19:25:42 +0200

    new encrypt - no progress bar support yet

--- BSK-Proj1-Logic/EncryptionWorker.cs
+++ BSK-Proj1-Logic/EncryptionWorker.cs
@@ -26,72 +26,60 @@ namespace BSK_Proj1_Logic
         {
             var outNameNoExtension = Path.GetFileNameWithoutExtension(outName);
             var path = Path.GetDirectoryName(fileName);
-            var outputName = outNameNoExtension + ".c3d.tmp.enc";
             var tdesKey = LoadTripleDESKey();
             var tdesIV = LoadTripleDESIV();
 
-            var fin = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            var foutTemp = new FileStream(outputName, FileMode.OpenOrCreate, FileAccess.Write);
-            var xmlOutputStream = new FileStream(Path.Combine(path, outNameNoExtension + ".xml"), FileMode.OpenOrCreate, FileAccess.Write);
-            foutTemp.SetLength(0);
-
-            var bin = new byte[100];         
-            var totlen = fin.Length;
-
-            var tdes = new TripleDESCryptoServiceProvider
+            using (var fin = new FileStream(fileName, FileMode.Open, FileAccess.Read))
             {
-                Mode = cipherMode
-            };
+                using (var xmlOutputStream = new FileStream(Path.Combine(path, outNameNoExtension + ".xml"), FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    var tdes = new TripleDESCryptoServiceProvider
+                    {
+                        Mode = cipherMode
+                    };
 
-            var encStream = new CryptoStream(foutTemp, tdes.CreateEncryptor(tdesKey, tdesIV), CryptoStreamMode.Write);
-            var fileInBase64 = ProcessFile(totlen, encStream, bin, fin);
-            encStream.Close();
+                    var encryptedText = EncryptFileTest(fin, tdes);
 
-            var fileExtension = Path.GetExtension(fileName);
+                    var fileExtension = Path.GetExtension(fileName);
 
-            XmlComposer.CreateXml(new List<UserModel> { new UserModel("asd", "123") }, Convert.ToBase64String(tdesIV), Convert.ToString(cipherMode), 
-                tdes.BlockSize, tdes.KeySize, "TripleDES", fileInBase64, fileExtension).Save(xmlOutputStream);
+                    XmlComposer.CreateXml(new List<UserModel> { new UserModel("asd", "123") }, Convert.ToBase64String(tdesIV), Convert.ToString(cipherMode),
+                        tdes.BlockSize, tdes.KeySize, "TripleDES", encryptedText, fileExtension).Save(xmlOutputStream);
+                }
+            }
 
-            xmlOutputStream.Close();
-
-            fin.Close();
-            foutTemp.Close();
-            File.Delete(outputName);
         }
 
         public void DecryptXmlFile(string fileName)
         {
-            var tdesKey = LoadTripleDESKey();
-            var tdesIV = LoadTripleDESIV();
+            //var tdesKey = LoadTripleDESKey();
+            //var tdesIV = LoadTripleDESIV();
 
-            var file = XElement.Load(fileName);
+            //var file = XElement.Load(fileName);
 
-            var dataInBase64 = file.Element("EncryptedData").Value;
-            var data = Convert.FromBase64String(dataInBase64);
-            var reader = new MemoryStream(data);
+            //var dataInBase64 = file.Element("EncryptedData").Value;
+            //var data = Convert.FromBase64String(dataInBase64);
+            //var reader = new MemoryStream(data);
 
-            var totLen = reader.Length;
+            //var totLen = reader.Length;
 
-            var cipherMode = file.Descendants().First(x => x.Name.LocalName == "CipherMode").Value;
-            var extension = file.Descendants().First(x => x.Name.LocalName == "FileExtension").Value;
+            //var cipherMode = file.Descendants().First(x => x.Name.LocalName == "CipherMode").Value;
+            //var extension = file.Descendants().First(x => x.Name.LocalName == "FileExtension").Value;
 
-            var outputFile = new FileStream(Path.GetFileNameWithoutExtension(fileName) + extension, FileMode.OpenOrCreate, FileAccess.Write);
-            outputFile.SetLength(0);
+            //var outputFile = new FileStream(Path.GetFileNameWithoutExtension(fileName) + extension, FileMode.OpenOrCreate, FileAccess.Write);
+            //outputFile.SetLength(0);
 
-            var bin = new byte[100];
+            //var tdes = new TripleDESCryptoServiceProvider
+            //{
+            //    Mode = HelperClass.GetCipherMode(cipherMode)
+            //};
 
-            var tdes = new TripleDESCryptoServiceProvider
-            {
-                Mode = HelperClass.GetCipherMode(cipherMode)
-            };
+            //var encStream = new CryptoStream(outputFile, tdes.CreateDecryptor(tdesKey, tdesIV), CryptoStreamMode.Write & CryptoStreamMode.Read);
 
-            var encStream = new CryptoStream(outputFile, tdes.CreateDecryptor(tdesKey, tdesIV), CryptoStreamMode.Write);
+            //ProcessFile(totLen, encStream, reader);
 
-            ProcessFile(totLen, encStream, bin, reader);
-
-            encStream.Close();
-            outputFile.Close();
-            reader.Close();
+            //encStream.Close();
+            //outputFile.Close();
+            //reader.Close();
         }
 
         // Load 3DES key from generated file
@@ -133,32 +121,71 @@ namespace BSK_Proj1_Logic
             return key;
         }
 
-        private string ProcessFile(long totlen, CryptoStream encStream, byte[] bin, Stream fin)
+        private string EncryptFileTest(Stream fin, TripleDESCryptoServiceProvider tdes)
         {
-            long rdlen = 0;
-            var prog = 0d;
-            while (rdlen < totlen)
+            // todo: make it a binary batch array processing method -> for big files and progress bar updates
+            var key = LoadTripleDESKey();
+            var iv = LoadTripleDESIV();
+
+            var bytes = new byte[fin.Length];
+
+            using (var ms = new MemoryStream())
             {
-                var len = fin.Read(bin, 0, 100);
-
-                encStream.Write(bin, 0, len);
-
-                rdlen = rdlen + len;
-                var progressValue = Math.Round(((double)rdlen / (double)totlen) * 100.0);
-
-                if (prog != progressValue)
-                {
-                    _backgroundWorker.ReportProgress((int)progressValue);
-                    prog = progressValue;
-                }
+                fin.CopyTo(ms);
+                bytes = ms.ToArray();
             }
-            var bytes = new byte[encStream.Length];
 
-            encStream.Read(bytes, 0, (int)encStream.Length);
+            var encryptedText = string.Empty;
 
-            var binaryString = Convert.ToBase64String(Encoding.UTF8.GetBytes(Encoding.UTF8.GetChars(bytes)));
+            using (var memoryStream = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(memoryStream, tdes.CreateEncryptor(key, iv), CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(bytes, 0, bytes.Length);
+                cryptoStream.FlushFinalBlock();
 
-            return binaryString;
+                var cipheredText = memoryStream.ToArray();
+                encryptedText = Convert.ToBase64String(cipheredText);
+            }
+
+            return encryptedText;
         }
+
+        //private string ProcessFile(long totlen, Stream fin, TripleDESCryptoServiceProvider tdes)
+        //{
+        //    long rdlen = 0;
+        //    var prog = 0d;
+
+        //    //var stringBuilder = new StringBuilder();
+
+            
+
+
+        //    while (rdlen < totlen)
+        //    {
+        //        var bin = new byte[100];
+
+        //        var len = fin.Read(bin, 0, 100);
+                
+        //        encStream.Write(bin, 0, len);
+                
+        //        rdlen = rdlen + len;
+        //        var progressValue = Math.Round(((double)rdlen / (double)totlen) * 100.0);
+
+        //        if (prog != progressValue)
+        //        {
+        //            _backgroundWorker.ReportProgress((int)progressValue);
+        //            prog = progressValue;
+        //        }
+
+        //        //var binaryString = Convert.ToBase64String(Encoding.UTF8.GetBytes(Encoding.UTF8.GetChars(bin)));
+        //        //stringBuilder.Append(binaryString);
+        //    }
+
+        //    var bytes = new byte[encStream.Length];
+        //    encStream.Read(bytes, 0, (int)encStream.Length);
+        //    var binaryString = Convert.ToBase64String(Encoding.UTF8.GetBytes(Encoding.UTF8.GetChars(bytes)));
+
+        //    return binaryString;
+        //}
     }
 }
